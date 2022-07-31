b7dd554465d445f8fa28f9840bd78537b3ed3e53
Korkmz <hatice_hh-@hotmail.com>
2022-07-31 12:09:48 +0300

    add task modal added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -100,7 +100,7 @@
         </div>
        
         <v-card  outlined :elevation="0" class="pa-2 card-task mb-2" >
-             <v-row class="d-flex mb-0 mb-0 "  no-gutters>
+             <v-row class="d-flex mb-0 mb-0" no-gutters>
                   <div>
                      Finiliaze storyboard
                 
@@ -325,6 +325,55 @@
     </v-row>
 
  
+   <v-dialog
+      v-model="dialog"
+      scrollable
+      max-width="700px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Open Dialog
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>  
+            <v-icon class="mr-1" color="#d1d1d1">  mdi-plus </v-icon> Add Task</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text class="pt-2">
+         <v-form ref="form" v-model="valid" lazy-validation >
+       
+
+            <v-text-field size="sm" label="Title" class="mb-2" hide-details dense outlined required ></v-text-field>
+            <v-text-field label="Description" class="mb-2" hide-details dense outlined required ></v-text-field>
+            <v-select v-model="select" :items="items" :error-messages="errors" label="User" data-vv-name="select" class="mb-2" hide-details  dense outlined required ></v-select>
+            <v-select v-model="select" :items="items" :error-messages="errors" label="Project" data-vv-name="select" class="mb-2" hide-details  dense outlined required ></v-select>
+            <v-select v-model="select" :items="items" :error-messages="errors" label="Priority" data-vv-name="select"  class="mb-2" hide-details dense outlined required ></v-select>
+
+         </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+
+          <v-spacer></v-spacer>
+          <v-btn depressed
+            color="blue "
+            text
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn color="primary"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
 </div>
 
@@ -376,4 +425,12 @@ body,.text-card-title{
 
   font-size: 13px;
 }
+
+.theme--light.v-text-field--outlined:not(.v-input--is-focused):not(.v-input--has-state) > .v-input__control > .v-input__slot fieldset {
+    color: rgb(204 204 204 / 38%);
+}
+
+ .v-text-field input {
+    font-size: 0.85em
+  }
 </style>
